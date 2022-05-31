9a7b4c7fcaa935a28b1d7ebb1c5618e1cd001cf1
keepwalking86 <nguyenvodung@gmail.com>
2022-05-31 18:49:51 +0700

    Init

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Storage;
+
+
+
+class UserController extends Controller
+
+{
+
+    /**
+
+     * Display a listing of the resource.
+
+     *
+
+     * @return \Illuminate\Http\Response
+
+     */
+
+    public function index(Request $request)
+
+    {
+
+        Storage::put('example.txt', 'Contents');
+
+
+
+        return view('users');
+
+    }
+
+}
